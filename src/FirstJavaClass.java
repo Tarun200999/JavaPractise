df6b9833437d49789db7027f0c9c537259db6094
Tarun200999 <tk4977163@gmail.com>
2022-06-17 16:48:02 +0530

    Update

--- src/FirstJavaClass.java
+++ src/FirstJavaClass.java
@@ -1,10 +1,22 @@
 import java.util.*;
-public class FirstJavaClass {
-    public static void main(String args[]){
-        Scanner scn=new Scanner(System.in);
-        int a=scn.nextInt();
-        int b=scn.nextInt();
 
-        System.out.println(a+b);
+public class FirstJavaClass {
+  public static void main(String args[]) {
+    Scanner scn = new Scanner(System.in);
+
+    String x="Hello";
+
+    String y="Hello";
+
+
+
+    System.out.println(x+y);
+      try {
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        System.out.println(a + b);
+      } catch (Exception e) {
+        System.out.println(e.toString());
+      }
     }
-}
+  }
